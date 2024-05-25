aa55c4a0742eeea3295017cbb35b5b3b91bb3da0
Kupferbart <sebastian.korzenski+github@gmail.com>
2024-05-25 15:14:40 +0200

    Passe App und main Methoden an

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,14 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'features/user_management/presentation/login_page.dart';
+
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final ColorScheme colorScheme = ColorScheme.fromSeed(seedColor: Colors.teal);
+
     return MaterialApp(
-      home: Container(
-        color: Colors.lightGreen,
+      theme: ThemeData(
+        listTileTheme: ListTileThemeData(
+          tileColor: colorScheme.surfaceContainerHigh,
+          textColor: colorScheme.onSurface,
+          selectedTileColor: colorScheme.secondaryContainer,
+          selectedColor: colorScheme.onSecondaryContainer,
+        ),
+        colorScheme: colorScheme,
+        useMaterial3: true,
       ),
+      home: const LoginPage(),
     );
   }
 }
