b3e21125106548e6db7afbd7ea9c69d87302b4fc
Kupferbart <sebastian.korzenski+github@gmail.com>
2024-05-25 19:16:40 +0200

    Nutze Router für Navigation

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,27 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kuka_companion/src/routing/app_router.dart';
 
-import 'features/companion/presentation/companion_page.dart';
-
-class App extends StatelessWidget {
+class App extends ConsumerWidget{
   const App({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final appRouter = ref.watch(appRouterProvider);
+
     final ColorScheme colorScheme =
         ColorScheme.fromSeed(seedColor: Colors.teal);
 
-    return MaterialApp(
-      theme: ThemeData(
-        listTileTheme: ListTileThemeData(
-          tileColor: colorScheme.surfaceContainerHigh,
-          textColor: colorScheme.onSurface,
-          selectedTileColor: colorScheme.secondaryContainer,
-          selectedColor: colorScheme.onSecondaryContainer,
-        ),
-        colorScheme: colorScheme,
-        useMaterial3: true,
+    final ThemeData theme = ThemeData(
+      listTileTheme: ListTileThemeData(
+        tileColor: colorScheme.surfaceContainerHigh,
+        textColor: colorScheme.onSurface,
+        selectedTileColor: colorScheme.secondaryContainer,
+        selectedColor: colorScheme.onSecondaryContainer,
       ),
-      home: const CompanionPage(),
+      colorScheme: colorScheme,
+      useMaterial3: true,
+    );
+
+    return MaterialApp.router(
+      routerConfig: appRouter,
+      theme: theme,
     );
   }
 }
